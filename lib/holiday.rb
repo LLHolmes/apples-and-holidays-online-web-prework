83e97f8410d65d53f9f33de484e52f355610dbf4
Lana Holmes <lana.lee.holmes@gmail.com>
2019-01-10 02:20:11 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -58,7 +58,8 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
-  listed_holiday = ""
+  
+  #listed_holiday = ""
   holiday_hash.each do |season, events|
     puts "#{season.to_s.capitalize}:"
     events.each do |holiday, gear|
