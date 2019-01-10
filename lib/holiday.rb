4b66a7b74d1c0a65e8a1d47d42fbc9c8686d0f9d
Lana Holmes <lana.lee.holmes@gmail.com>
2019-01-10 01:30:01 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -47,7 +47,7 @@ end
 
 def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
-  holiday_hash[:winter].collect { |holiday, 
+  holiday_hash[:winter].collect { |holiday, gear| gear}.flatten
 end
 
 def all_supplies_in_holidays(holiday_hash)
